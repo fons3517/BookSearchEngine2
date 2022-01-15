412234c6b6a50316ff13e12e86d3f440fc298cc4
Alfonso Robles <fons3517@gmail.com>
2022-01-15 16:07:40 -0600

    added GETME to queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,23 @@
 import { gql } from '@apollo/client';
 
+export const GET_ME = gql`
+  query getMe {
+    me {
+      username
+      email
+      password
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+    }
+  }
+`;
+
 export const QUERY_BOOKS = gql`
   query getBooks {
     books {
